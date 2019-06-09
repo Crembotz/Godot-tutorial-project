062302c6a96597ec953ab01f6b8ba23d554607cb
unknown <erans995@gmail.com>
2019-06-09 19:56:36 +0300

    added animations

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -7,17 +7,25 @@ const UP = Vector2(0,-1)
 const GRAVITY = 20
 const JUMP_FORCE = -500
 var motion = Vector2(0,0)
-var horizontalSpeed = 165
+var horizontalSpeed = 120
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	motion.y+=GRAVITY	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = horizontalSpeed
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-			motion.x = -horizontalSpeed
-	else: motion.x=0
+		motion.x = -horizontalSpeed
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
+	else: 
+		motion.x=0
+		$Sprite.play("Idle")
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_FORCE
-	else: motion.y+=GRAVITY		
+	else: 
+		$Sprite.play("Jump")	
 	motion = move_and_slide(motion,UP)
 	pass
