7eacce46367dee7f6b689533c307cf5a429df05d
Eran Simhon <erans995@gmail.com>
2019-06-10 20:49:03 +0300

    improved movement and added a new animation

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,29 +3,38 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+const  ACCELERATION = 50
 const UP = Vector2(0,-1)
 const GRAVITY = 20
 const JUMP_FORCE = -500
 var motion = Vector2(0,0)
-var horizontalSpeed = 120
+var maxHorizontalSpeed = 120
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var friction = false
 	motion.y+=GRAVITY	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = horizontalSpeed
+		motion.x = min(motion.x+ACCELERATION,maxHorizontalSpeed)
 		$Sprite.flip_h = false
 		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -horizontalSpeed
+		motion.x = max(motion.x-ACCELERATION,-maxHorizontalSpeed)
 		$Sprite.flip_h = true
 		$Sprite.play("Run")
 	else: 
-		motion.x=0
 		$Sprite.play("Idle")
+		friction = true 
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_FORCE
+		if(friction==true):
+			motion.x=lerp(motion.x,0,0.2)		
 	else: 
-		$Sprite.play("Jump")	
+		if(motion.y<0):
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if(friction==true):
+			motion.x=lerp(motion.x,0,0.05)	
 	motion = move_and_slide(motion,UP)
 	pass
